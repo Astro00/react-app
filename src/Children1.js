014ba473e60d2065e0ba8f1643ed018f8742cca5
jianglei <jarrelljl@163.com>
2018-03-05 22:00:20 +0800

    add children components

---
+++ src/Children1.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Children1 extends Component{
+    render(){
+        return <h3>this is children1</h3>
+    }
+}
+
+export default Children1;
---
+++ src/Children2.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Children2 extends Component {
+    render() {
+        return <h3>this is children2</h3>
+    }
+}
+
+export default Children2;
